1eaed084863c4b0554013a26b37be569327ccfdc
CodingOverload <64263162+CodingOverload@users.noreply.github.com>
2021-04-22 20:56:03 +0800

    fixed json

--- js/createPanel.js
+++ js/createPanel.js
@@ -1,19 +1,26 @@
 $(document).ready(function(){
-    $('form').submit(function(e){
+    $form = $('form');
+
+    //load users stored files
+    $.getJSON("../php/createPanel.php", function(data){
+        console.log(data);
+    });
+
+    $form.submit(function(e){
+        //stop form from submitting
         e.preventDefault();
 
-        var name = document.getElementById('panelname').value.trim();
-        var myData = {panelname: name};
-        $.ajax(
-        {
-            type: 'post',
-            url: '../php/createPanel.php',
-            dataType: 'json',
-            data: {json: JSON.stringify(myData)},
-        },
-        function(datarec){
-            alert(datarec);
-        });
-        e.preventDefault();
+        //get the users input for panel name
+        var name = document.getElementById('panelname');
+
+        //check if panel name is empty or null
+        if(name.value.trim() == '' || name.value.trim() == null){
+            alert('Panel must have a name');
+            name.focus();
+        }
+        else{
+            //submit form
+            $form.off().submit();
+        }
     });
 });
--- php/createPanel.php
+++ php/createPanel.php
@@ -1,3 +1,26 @@
 <?php
-    echo json_decode($_POST['json']);
+$servername = "127.0.0.1";
+$username = "ryan";
+$password = "Angusdraper2";
+$db = "my_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  echo "error connecting to database";
+}
+else{
+    $sql = "SELECT tablename, filename FROM files";
+    $result = mysqli_query($conn, $sql);
+
+    $arr = array();
+
+    while($row = mysqli_fetch_assoc($result)){
+        $arr[] = $row;
+    }
+
+    echo json_encode($arr);
+}
 ?>
